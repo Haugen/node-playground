5f4ada88675c8cc9e0431801d2e69112c49734d6
Tobias Haugen <hej@tobiashaugen.se>
2018-09-30 19:51:14 -0300

    Create and list tasks.

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,18 +1,77 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var assert = require('assert');
 
-/* GET home page. */
+var dbUrl = 'mongodb://localhost:27017/test';
+
+// GET home page.
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Validation and sessions',
-    success: req.session.success,
-    errors: req.session.errors,
-    success: req.session.success
-  });
-  req.session.errors = req.session.success = null;
+  let results = [];
+  // Connect to the mongo db with the dbUrl.
+  mongo.connect(dbUrl, function(err, client) {
+    // In the first callback we get all the items from the 'tasks' collection.
+    assert.equal(null, err);
+    let db = client.db('test');
+    let cursor = db.collection('tasks').find();
+    // Then iterate over the db items and push each one into an array.
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      results.push(doc);
+    }, function() {
+      // The second callback will be called once the first one finishes. Here we
+      // close the connection and then render the front page with the results.
+      client.close();
+      res.render('index', {
+        title: 'Simple TODO!',
+        success: req.session.success,
+        errors: req.session.errors,
+        items: results
+      });
+      req.session.errors = req.session.success = null;
+    });
+  })
 });
 
-router.post('/submit', function(req, res, next) {
+// Inserting data.
+router.post('/insert', function(req, res, next) {
+  // If the task textfield is empty, redirect to front page with an error.
+  if (!req.body.task) {
+    req.session.errors = {error: { msg: "You need to enter a task." }};
+    res.redirect('/');
+  } else {
+    // create an item object from the input fields.
+    let item = {
+      task: req.body.task,
+      prio: req.body.prio
+    };
+
+    // Connect to the database and inser the new item.
+    mongo.connect(dbUrl, function(err, client) {
+      assert.equal(null, err);
+      let db = client.db('test');
+      db.collection('tasks').insertOne(item, function(err, result) {
+        assert.equal(null, err);
+        client.close();
+      });
+    });
+
+    res.redirect('/');
+  }
+});
+
+// Updating data.
+router.post('/update', function(req, res, next) {
+
+});
+
+// Deleting data.
+router.post('/delete', function(req, res, next) {
+
+});
+
+/* Old POST for the homepage. Practice with a submitted form. */
+router.post('/old-submit', function(req, res, next) {
   req.check('email', 'Incorrect e-mail address.').isEmail();
   req.check('password', 'Try again with password, caballero!')
     .isLength({min: 4})
@@ -27,23 +86,6 @@ router.post('/submit', function(req, res, next) {
     req.session.success = true;
   }
   res.redirect('/')
-});
-
-/* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-/* GET a test page. */
-router.get('/test/:id/:name', function(req, res, next) {
-  res.render('test', { id: req.params.id, name: req.params.name });
-});
-
-/* POST to a test page. */
-router.post('/test/submit', function(req, res, next) {
-  let id = req.body.id;
-  let name = req.body.name;
-  res.redirect(`/test/${id}/${name}`);
 });
 
 module.exports = router;
