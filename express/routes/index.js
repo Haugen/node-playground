56dd1032492db9f6d3095e34fe68ca905756e30f
Tobias Haugen <hej@tobiashaugen.se>
2018-10-01 14:25:13 -0300

    List and add implemented with Mongoose.

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,30 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+
+var taskDataSchema = new mongoose.Schema({
+  task: {type: String, required: true},
+  prio: String
+}, {
+  collection: 'tasks'
+});
+
+var taskData = mongoose.model('taskData', taskDataSchema);
 
 // GET home page.
 router.get('/', function(req, res, next) {
-
+  taskData.find().then(function(doc) {
+    res.render('index', {
+      items: doc,
+      errors: req.session.error
+    });
+    req.session.error = null;
+  });
 });
 
 // Inserting data.
 router.post('/insert', function(req, res, next) {
-  // If the task textfield is empty, redirect to front page with an error.
-  if (!req.body.task) {
-    req.session.errors = {error: { msg: "You need to enter a task." }};
+  let item = {
+    task: req.body.task,
+    prio: req.body.prio
+  };
+
+  let data = new taskData(item);
+  data.save(function(err) {
+    // TODO Figure out how to handle session errors more dynamically.
+    if (err) req.session.error = { error: {msg: err.errors.task.message} };
     res.redirect('/');
-  } else {
-    // create an item object from the input fields.
-    let item = {
-      task: req.body.task,
-      prio: req.body.prio
-    };
-  }
+  });
 });
 
 // GET page for Updating data.
 router.get('/update/:id', function(req, res, next) {
   let id = req.params.id;
-  let result = {};
+  let task = taskData.findById(id, function(err, doc) {
+    if (err) {
+      // TODO Figure out how to handle session errors more dynamically.
+      req.session.error = { error: {msg: err.message} };
+      res.redirect('/');
+    } else {
+      res.render('update', { item: doc });
+    }
+  });
 });
 
 // POST page for updating the item in the db.
